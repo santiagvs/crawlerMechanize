699186a5f64f771c87175de73e42aa493cc81012
Santiago <victor.lacsilva0102@gmail.com>
2022-03-13 22:08:43 -0300

    feat: now it checks number of pages properly

--- app/services/crawler/scraper_service.rb
+++ app/services/crawler/scraper_service.rb
@@ -6,25 +6,31 @@ module Crawler
       agent = Mechanize.new
       
       quotes = []
-      page = agent.get("#{URL_HOST}/page/1/")
-      paginator = page.at('ul.pager li.next a').attributes['href'].value
-      
       page_num = 1
-
-      while page_num <= 10
+      page = pager(agent, page_num)
+        
+      loop do
         full_page = page.css('div.quote')
         full_page.each do |quote|
           quotes.push({
-              quote: quote.css('span.text').text,
+              quote: quote.css('span.text').text.tr('“”', ''),
               author: quote.css('small.author').text,
               tags: quote.css('div.tags a.tag').map(&:text),
           })
         end
         
+        total_pages = (quotes.count.to_f / page_num.to_f).ceil
+
+        break if page_num == total_pages
+
         page_num += 1
         page = agent.get("#{URL_HOST}/page/#{page_num}/")
       end
       quotes
+    end
+
+    def self.pager(agent, page_num)
+      agent.get("#{URL_HOST}/page/#{page_num}/")
     end
   end
 end
