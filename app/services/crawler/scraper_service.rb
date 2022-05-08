4622282c5efe4f3daf1c54b1d8aa590a65b5c804
Santiago <victor.lacsilva0102@gmail.com>
2022-05-08 16:08:03 -0300

    refactor: refactoring crawler DRY

--- app/services/crawler/scraper_service.rb
+++ app/services/crawler/scraper_service.rb
@@ -1,36 +1,56 @@
 module Crawler
+  # scrape service
   class ScraperService
-    URL_HOST = 'http://quotes.toscrape.com'
-    
-    def self.parser       
-      agent = Mechanize.new
-      
-      quotes = []
-      page_num = 1
-      page = pager(agent, page_num)
-        
-      loop do
-        full_page = page.css('div.quote')
-        full_page.each do |quote|
-          quotes.push({
+    class << self
+      URL_HOST = 'http://quotes.toscrape.com'.freeze
+
+      def search_quotes
+        agent = Mechanize.new
+        page_num = 1
+
+        quote_paginator(agent, page_num, request_page(agent, page_num))
+      end
+
+      private
+
+      def quote_paginator(agent, page_num, current_page)
+        quotes = []
+        loop do
+          quote_selector = current_page.css('div.quote')
+
+          data_page(quote_selector, quotes)
+
+          break if page_ended?(quotes, page_num)
+
+          page_num += 1
+          current_page = request_page(agent, page_num)
+        end
+
+        quotes
+      end
+
+      def data_page(quote_selector, quotes)
+        quote_selector.each do |quote|
+          quotes.push(
+            {
               quote: quote.css('span.text').text.tr('“”', ''),
               author: quote.css('small.author').text,
-              tags: quote.css('div.tags a.tag').map(&:text),
-          })
+              tags: quote.css('div.tags a.tag').map(&:text)
+            }
+          )
         end
-        
-        total_pages = (quotes.count.to_f / page_num.to_f).ceil
-
-        break if page_num == total_pages
-
-        page_num += 1
-        page = agent.get("#{URL_HOST}/page/#{page_num}/")
+        quotes
       end
-      quotes
-    end
 
-    def self.pager(agent, page_num)
-      agent.get("#{URL_HOST}/page/#{page_num}/")
+      def page_ended?(quotes, page_num)
+        total_pages = (quotes.count / page_num).to_f.ceil
+
+        total_pages == page_num
+      end
+
+      def request_page(agent, page_num)
+        agent.get("#{URL_HOST}/page/#{page_num}/")
+      end
     end
   end
 end
